b1bbc440c5c532b82e3633a8d324a81284b61e3a
soadaa <Soadhlh@gmail.com>
2024-01-11 19:33:44 -0500

    Events show index pages and links_to show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,22 @@
 class EventsController < ApplicationController
+before_action :set_event, only: %i[show]
 
   def index
     @time = "#{Time.now.asctime}"
     @events = Event.all
   end
 
+  def show
+   @creator = User.where(id: @event.user_id).first
+  end
+
+  def create
+    @event = Event.new
+  end
+
+  protected
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
